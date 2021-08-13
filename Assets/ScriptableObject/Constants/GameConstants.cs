af5f26ef766540316b1639f57197aad93f71496d
shiinx <z.shiinx@gmail.com>
2021-08-14 03:21:18 +0800

    remove files

--- Assets/ScriptableObject/Constants/GameConstants.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GameConstants", menuName = "ScriptableObjects/Constants/GameConstants")]
-public class GameConstants : ScriptableObject
-{
-
-}
--- Assets/Scripts/Enemy/EnemyInterface.cs
+++
@@ -1,7 +0,0 @@
-public interface EnemyInterface {
-    //declare method signatures
-    void TakeBulletDamage();
-    void TakeTurretDamage();
-    void TakeClaymoreDamage();
-    void KillSelf();
-}
