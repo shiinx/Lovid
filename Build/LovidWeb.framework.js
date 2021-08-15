e29cfd374d29321ebc9850335cba3a05219e1a62
shiinx <z.shiinx@gmail.com>
2021-08-16 05:50:39 +0800

    aasdasd

--- Build/LovidWeb.framework.js
+++ Build/LovidWeb.framework.js
@@ -1290,7 +1290,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2641680;
+STATICTOP = STATIC_BASE + 2641888;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3316,7 +3316,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2641680;
+var STATIC_BUMP = 2641888;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
